8db399533ac40faeec53a8db0703fba47c0194fa
Xelame <m2afkeb9@duck.com>
2023-11-27 10:49:42 +0100

    add auth page

--- lib/pages/auth.dart
+++ lib/pages/auth.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
+// Sign up page
 class AuthPage extends StatefulWidget {
   const AuthPage({super.key});
 
@@ -76,6 +77,87 @@ class _AuthPageState extends State<AuthPage> {
               ElevatedButton(
                 onPressed: _register,
                 child: const Text('S\'enregistrer'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+// sign in page
+class SignInPage extends StatefulWidget {
+  const SignInPage({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _SignInPageState createState() => _SignInPageState();
+}
+
+class _SignInPageState extends State<SignInPage> {
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  void _signIn() async {
+    try {
+      UserCredential userCredential =
+          await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: _emailController.text,
+        password: _passwordController.text,
+      );
+
+      // ignore: use_build_context_synchronously
+      Navigator.pop(context);
+
+      // Utilisateur connecté avec succès
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        const SnackBar(
+          content: Text('No user found for that email.'),
+        );
+      } else if (e.code == 'wrong-password') {
+        const SnackBar(
+          content: Text('Wrong password provided for that user.'),
+        );
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    const spacer = SizedBox(height: 20);
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text('Authentification'),
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              TextFormField(
+                controller: _emailController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Email',
+                ),
+              ),
+              spacer,
+              SizedBox(
+                child: TextFormField(
+                  controller: _passwordController,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Mot de passe',
+                  ),
+                ),
+              ),
+              spacer,
+              ElevatedButton(
+                onPressed: _signIn,
+                child: const Text('Se connecter'),
               ),
             ],
           ),
